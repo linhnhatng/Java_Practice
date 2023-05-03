fe182e60b795788a61bd4cb93f462ed49d14fe78
linhnhatng <121186834+linhnhatng@users.noreply.github.com>
2023-05-03 15:04:06 +0700

    Add files via upload

---
+++ javaCore/src/main/java/LinhNTN11_W5/Person.java
@@ -0,0 +1,41 @@
+package LinhNTN11_W5;
+
+public class Person {
+  private String name;
+  private int yearOfBirth;
+  private int phoneNumber;
+  private String address;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getYearOfBirth() {
+    return yearOfBirth;
+  }
+
+  public void setYearOfBirth(int yearOfBirth) {
+    this.yearOfBirth = yearOfBirth;
+  }
+
+  public int getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(int phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+}
---
+++ javaCore/src/main/java/LinhNTN11_W5/TestSubstring2.java
@@ -0,0 +1,12 @@
+package LinhNTN11_W5;
+
+import static jdk.nashorn.internal.objects.NativeString.substring;
+
+
+    public class TestSubstring2 {
+        public static void main(String args[]) {
+            String s = "Hello I'm Java";
+            System.out.println(s.substring(0,4));
+        }
+    }
+
